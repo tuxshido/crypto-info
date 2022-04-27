f4ad330751600cc27847d357f016c90e1cede7e9
Charles <c.metman@e.email.com>
2022-04-27 15:25:13 +0900

    trying to fix the routing issue for the depoilement on the github pages

--- src/Coin.js
+++ src/Coin.js
@@ -3,51 +3,52 @@ import { useNavigate } from "react-router-dom";
 import "./Coin.css";
 
 const Coin = ({
-    id,
-    name,
-    price,
-    symbol,
-    marketcap,
-    volume,
-    image,
-    priceChange,
+	id,
+	name,
+	price,
+	symbol,
+	marketcap,
+	volume,
+	image,
+	priceChange,
 }) => {
-    const navigate = useNavigate();
-    return (
-        <div className="coin-container">
-            <div
-                className="coin-row"
-                onClick={() => {
-                    navigate(`/#/crypto/${id}`);
-                }}>
-                <div className="coin">
-                    <img src={image} alt="crypto" />
-                    <h1>{name}</h1>
-                    <p className="coin-symbol">{symbol}</p>
-                </div>
-                <div className="coin-data">
-                    <p className="coin-price">${price}</p>
-                    <p className="coin-volume">${volume.toLocaleString()}</p>
+	const navigate = useNavigate();
+	return (
+		<div className="coin-container">
+			<div
+				className="coin-row"
+				onClick={() => {
+					navigate(`/crypto-info/crypto/${id}`);
+				}}
+			>
+				<div className="coin">
+					<img src={image} alt="crypto" />
+					<h1>{name}</h1>
+					<p className="coin-symbol">{symbol}</p>
+				</div>
+				<div className="coin-data">
+					<p className="coin-price">${price}</p>
+					<p className="coin-volume">${volume.toLocaleString()}</p>
 
-                    {priceChange && priceChange < 0 ? (
-                        <p className="coin-percent red">
-                            {priceChange.toFixed(2)}%
-                        </p>
-                    ) : priceChange ? (
-                        <p className="coin-percent green">
-                            {priceChange.toFixed(2)}%
-                        </p>
-                    ) : (
-                        <p className="coin-percent green">0%</p>
-                    )}
+					{priceChange && priceChange < 0 ? (
+						<p className="coin-percent red">
+							{priceChange.toFixed(2)}%
+						</p>
+					) : priceChange ? (
+						<p className="coin-percent green">
+							{priceChange.toFixed(2)}%
+						</p>
+					) : (
+						<p className="coin-percent green">0%</p>
+					)}
 
-                    <p className="coin-marketcap">
-                        ${marketcap.toLocaleString()}
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
+					<p className="coin-marketcap">
+						${marketcap.toLocaleString()}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Coin;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 function NavBar(props) {
-    return (
-        <nav className="navbar">
-            <Link to="/">
-                <button className="homeBtn">Home</button>
-            </Link>
-            <Link to="/">
-                <h1>Crypto Info</h1>
-            </Link>
-        </nav>
-    );
+	return (
+		<nav className="navbar">
+			<Link to="/crypto-info/">
+				<button className="homeBtn">Home</button>
+			</Link>
+			<Link to="/crypto-info/">
+				<h1>Crypto Info</h1>
+			</Link>
+		</nav>
+	);
 }
 
 export default NavBar;
